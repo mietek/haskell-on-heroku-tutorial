975681b0fe28305234de5129721b8aca930b045e
Miëtek Bak <mietek@bak.io>
2015-01-06 07:56:40 +0000

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,68 @@
+import Control.Concurrent.STM
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Proxy
+import Data.Text
+import GHC.Generics
+import Network.Wai.Handler.Warp
+import Servant
+import System.Environment
+import System.IO
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+
+newtype Note = Note
+    { contents :: Text
+    }
+  deriving (Generic, Show)
+
+instance FromJSON Note
+instance ToJSON Note
+
+
+emptyNotes :: IO (TVar [Note])
+emptyNotes =
+    newTVarIO []
+
+getNotes :: MonadIO m => TVar [Note] -> m [Note]
+getNotes notes =
+    liftIO $ readTVarIO notes
+
+postNote :: MonadIO m => TVar [Note] -> Note -> m [Note]
+postNote notes note =
+    liftIO $ do
+      T.putStrLn $ contents note
+      atomically $ do
+        oldNotes <- readTVar notes
+        let newNotes = note : oldNotes
+        writeTVar notes newNotes
+        return newNotes
+
+
+type NoteAPI =
+         Get Text
+    :<|> "notes" :> Get [Note]
+    :<|> "notes" :> ReqBody Note :> Post [Note]
+
+noteAPI :: Proxy NoteAPI
+noteAPI =
+    Proxy
+
+server :: Text -> TVar [Note] -> Server NoteAPI
+server home notes =
+         return home
+    :<|> getNotes notes
+    :<|> postNote notes
+
+
+main :: IO ()
+main = do
+    hSetBuffering stdout LineBuffering
+    env <- getEnvironment
+    let port = maybe 8080 read $ lookup "PORT" env
+        home = maybe "Welcome to Haskell on Heroku" T.pack $
+                 lookup "TUTORIAL_HOME" env
+    notes <- emptyNotes
+    run port $ serve noteAPI $ server home notes
---
+++ README.md
@@ -0,0 +1,37 @@
+_haskell-on-heroku-tutorial_
+============================
+
+Companion app for the [Haskell on Heroku tutorial](https://haskellonheroku.com/tutorial/).
+
+Simple Haskell web service, built with [_servant_](https://hackage.haskell.org/package/servant).
+
+
+Usage
+-----
+
+```
+$ PORT=8080 haskell-on-heroku-tutorial
+```
+
+
+### Installation
+
+Installs in one command on most systems, using [Halcyon](https://halcyon.sh/):
+
+```
+$ halcyon install https://github.com/mietek/haskell-on-heroku-tutorial
+```
+
+
+### Deployment
+
+Deploys in one click to a new [DigitalOcean](https://digitalocean.com/) droplet, or to the [Heroku](https://heroku.com/) platform:
+
+- [Deploy to DigitalOcean](https://halcyon.sh/deploy/?url=https://github.com/mietek/haskell-on-heroku-tutorial)
+- [Deploy to Heroku](https://heroku.com/deploy?template=https://github.com/mietek/haskell-on-heroku-tutorial)
+
+
+About
+-----
+
+Made by [Miëtek Bak](https://mietek.io/).  Published under the [MIT X11 license](https://mietek.io/license/).
